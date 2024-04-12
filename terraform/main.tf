eec8a38cb7e3086e5f4253da80b68624e336a16e
GomathiselviS <gomathiselvi@gmail.com>
2024-04-12 15:33:54 -0400

    Configuration steps to tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,10 +16,65 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames = true
+}
+
+# Create a public subnet for bastion
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.1.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "tf_demo"
+  }
+}
+
+# Create security group for bastion
+resource "aws_security_group" "sec_grp" {
+  name        = "ansible-test-sg"
+  description = "security group for bastion"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name = "tf_demo"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sec_ingress_rule1" {
+  security_group_id = aws_security_group.sec_grp.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sec_ingress_rule2" {
+  security_group_id = aws_security_group.sec_grp.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5000
+  ip_protocol       = "tcp"
+  to_port           = 5000
+}
+
+resource "aws_vpc_security_group_egress_rule" "sec_egress_rule" {
+  security_group_id = aws_security_group.sec_grp.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 resource "aws_instance" "tf-demo-aws-ec2-instance-a" {
   ami           = "ami-00868b88dcd97faed"
   instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnet.id
+  key_name = "ansible-test-key"
   ebs_optimized = true
+  vpc_security_group_ids = [aws_security_group.sec_grp.id]
+  associate_public_ip_address = true
+  user_data = file("install.sh")
   tags = {
     Name = "tf-demo-aws-ec2-instance-a"
   }
